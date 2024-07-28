b4229a68842578b645f0dbdb28117c32f6627022
sniffjoke <sniffjoke@gmail.com>
2024-07-28 18:39:30 +0200

    update for tests

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -14,11 +14,10 @@ export type OutputErrorsType = {
 export const errorMiddleware = (req: Request, res: Response<OutputErrorsType>, next: NextFunction) => {
     const e = validationResult(req)
     if (!e.isEmpty()) {
-        const eArray = e.array({onlyFirstError: true}) as { path: FieldNamesType, msg: string }[]
-        console.log(eArray)
+        const eArray = e.array({onlyFirstError: true}) as { path: FieldNamesType, msg: string, location: string }[]
         res
             // .status(eArray && eArray[0].path === 'name' ? 400 : 404)
-            .status(eArray && eArray.find(x => x.msg === 'Блог с заданным id не найден!' || 'Пост с заданным id не найден!') ? 404 : 400)
+            .status(eArray && eArray.find(x => x.location === 'params') ? 404 : 400)
             .json({
                 errorsMessages: eArray.map(x => ({field: x.path, message: x.msg}))
             })
