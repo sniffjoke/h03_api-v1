5f919c1202d9494d09a4880d397c469bea24450d
sniffjoke <sniffjoke@gmail.com>
2024-07-28 18:17:00 +0200

    update for tests

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -15,10 +15,10 @@ export const errorMiddleware = (req: Request, res: Response<OutputErrorsType>, n
     const e = validationResult(req)
     if (!e.isEmpty()) {
         const eArray = e.array({onlyFirstError: true}) as { path: FieldNamesType, msg: string }[]
-
+        console.log(eArray)
         res
             // .status(eArray && eArray[0].path === 'name' ? 400 : 404)
-            .status(eArray && eArray.find(x => x.path === 'name' || 'websiteUrl') ? 400 : 404)
+            .status(eArray && eArray.find(x => x.path === '_id') ? 404 : 400)
             .json({
                 errorsMessages: eArray.map(x => ({field: x.path, message: x.msg}))
             })
